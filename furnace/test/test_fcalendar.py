07b840dfec069e7287c6813d81c7fcfffe98c08f
John Graham <john.a.graham@gmail.com>
2014-10-22 22:13:12 -0500

    clean up test syntax

--- furnace/test/test_fcalendar.py
+++ furnace/test/test_fcalendar.py
@@ -2,7 +2,6 @@
 Test the fcalendar module
 """
 
-import unittest
 from furnace.data import fcalendar, yahoo
 import datetime
 import itertools
@@ -10,87 +9,68 @@ import os.path
 import pickle
 from numpy import datetime64
 
-#pylint: disable=R0904
-class TestFcalendar(unittest.TestCase):
-    """ Test various dates in the financial calendaring system """
+def test_weekends():
+    """ Test that weekends don't fall in trading dates """
+    trading_dates = fcalendar.build_trading_date_rule(datetime.datetime(2001, 1, 1))
 
-    def setUp(self):
-        """ Initialize fixture """
-        self.trading_dates = fcalendar.build_trading_date_rule(datetime.datetime(2001, 1, 1))
+    assert not datetime.datetime(2002, 3, 9) in trading_dates
+    assert not datetime.datetime(2002, 3, 10) in trading_dates
 
-    def test_weekends(self):
-        """ Test that weekends don't fall in trading dates """
+def test_holidays():
+    """ Test that some holidays aren't treated as trading dates """
+    trading_dates = fcalendar.build_trading_date_rule(datetime.datetime(2001, 1, 1))
 
-        self.assertFalse(datetime.datetime(2002, 3, 9) in self.trading_dates)
-        self.assertFalse(datetime.datetime(2002, 3, 10) in self.trading_dates)
+    assert not datetime.datetime(2003, 12, 25) in trading_dates
+    assert not datetime.datetime(2003, 5, 26) in trading_dates # memorial day
 
-    def test_holidays(self):
-        """ Test that some holidays aren't treated as trading dates """
+def test_weekdays():
+    """ Test that normal weekdays are trading dates """
+    trading_dates = fcalendar.build_trading_date_rule(datetime.datetime(2001, 1, 1))
 
-        self.assertFalse(datetime.datetime(2003, 12, 25) in self.trading_dates)
-        self.assertFalse(datetime.datetime(2003, 5, 26) in self.trading_dates) # memorial day
+    def make_date(year, month, day):
+        """ Helper to create numpy datetimes """
+        return datetime64(datetime.datetime(year, month, day))
 
-    def test_weekdays(self):
-        """ Test that normal weekdays are trading dates """
+    assert make_date(2004, 5, 21) in trading_dates.values
+    assert make_date(2004, 5, 17) in trading_dates.values
+    assert make_date(2004, 5, 18) in trading_dates.values
+    assert make_date(2004, 5, 19) in trading_dates.values
+    assert make_date(2004, 5, 20) in trading_dates.values
 
-        def make_date(year, month, day):
-            """ Helper to create numpy datetimes """
-            return datetime64(datetime.datetime(year, month, day))
+def test_after():
+    """ Test the after logic """
+    date = datetime.datetime
+    calendar = fcalendar.make_fcalendar(datetime.datetime(2000, 1, 1))
 
-        self.assertTrue(make_date(2004, 5, 21) in self.trading_dates.values)
-        self.assertTrue(make_date(2004, 5, 17) in self.trading_dates.values)
-        self.assertTrue(make_date(2004, 5, 18) in self.trading_dates.values)
-        self.assertTrue(make_date(2004, 5, 19) in self.trading_dates.values)
-        self.assertTrue(make_date(2004, 5, 20) in self.trading_dates.values)
-#pylint: enable=R0904
+    assert calendar.nth_trading_day_after(3, date(2005, 6, 13)) == date(2005, 6, 16)
+    assert calendar.nth_trading_day_after(0, date(2005, 6, 13)) == date(2005, 6, 13)
+    assert calendar.nth_trading_day_after(0, date(2005, 6, 18)) == date(2005, 6, 20)
 
-#pylint: disable=R0904
-class TestFcalendarHelpers(unittest.TestCase):
-    """ Test the before and after helpers """
+def test_before():
+    """ Test the before logic """
+    date = datetime.datetime
+    calendar = fcalendar.make_fcalendar(datetime.datetime(2000, 1, 1))
 
-    def test_after(self):
-        """ Test the after logic """
+    #july forth is this week
+    assert calendar.nth_trading_day_before(3, date(2006, 7, 7)) == date(2006, 7, 3)
+    assert calendar.nth_trading_day_before(0, date(2006, 7, 7)) == date(2006, 7, 7)
+    assert calendar.nth_trading_day_before(0, date(2006, 7, 8)) == date(2006, 7, 7)
 
-        date = datetime.datetime
-        calendar = fcalendar.make_fcalendar(datetime.datetime(2000, 1, 1))
+def test_range():
+    """ Tests that the range of the calendar covers the same range as the data """
+    begin_date = datetime.datetime(2000, 1, 1)
+    end_date = datetime.datetime.today()
 
-        self.assertTrue(calendar.nth_trading_day_after(3, date(2005, 6, 13)) == date(2005, 6, 16))
-        self.assertTrue(calendar.nth_trading_day_after(0, date(2005, 6, 13)) == date(2005, 6, 13))
-        self.assertTrue(calendar.nth_trading_day_after(0, date(2005, 6, 18)) == date(2005, 6, 20))
+    if os.path.isfile("spy_price_cache_" + str(datetime.date.today()) + ".csv"):
+        dates_available = pickle.load(open("spy_price_cache_" + str(datetime.date.today()) + ".csv", "r"))
+    else:
+        prices_available = yahoo.webload_symbol_price("SPY", begin_date, end_date)
+        dates_available = set(timestamp.to_pydatetime() for timestamp in prices_available.index.tolist())
+        pickle.dump(dates_available, open("spy_price_cache_" + str(datetime.date.today()) + ".csv", "w"))
 
-    def test_before(self):
-        """ Test the before logic """
+    calendar = fcalendar.make_fcalendar(begin_date)
+    dates_expected = set([day for day in itertools.takewhile(lambda d: d <= end_date, [x for x in calendar])])
 
-        date = datetime.datetime
-        calendar = fcalendar.make_fcalendar(datetime.datetime(2000, 1, 1))
+    dates_misaligned = dates_available.symmetric_difference(dates_expected)
 
-        #july forth is this week
-        self.assertTrue(calendar.nth_trading_day_before(3, date(2006, 7, 7)) == date(2006, 7, 3))
-        self.assertTrue(calendar.nth_trading_day_before(0, date(2006, 7, 7)) == date(2006, 7, 7))
-        self.assertTrue(calendar.nth_trading_day_before(0, date(2006, 7, 8)) == date(2006, 7, 7))
-#pylint: enable=R0904
-
-#pylint: disable=R0904
-#NOTE: too many public methods
-class TestFCalendarRange(unittest.TestCase):
-    """ Test that the range of the calendar is correct """
-
-    def test_range(self):
-        """ Tests that the range of the calendar covers the same range as the data """
-        begin_date = datetime.datetime(2000, 1, 1)
-        end_date = datetime.datetime.today()
-
-        if os.path.isfile("spy_price_cache_" + str(datetime.date.today()) + ".csv"):
-            dates_available = pickle.load(open("spy_price_cache_" + str(datetime.date.today()) + ".csv", "r"))
-        else:
-            prices_available = yahoo.webload_symbol_price("SPY", begin_date, end_date)
-            dates_available = set(timestamp.to_pydatetime() for timestamp in prices_available.index.tolist())
-            pickle.dump(dates_available, open("spy_price_cache_" + str(datetime.date.today()) + ".csv", "w"))
-
-        calendar = fcalendar.make_fcalendar(begin_date)
-        dates_expected = set([day for day in itertools.takewhile(lambda d: d <= end_date, [x for x in calendar])])
-
-        dates_misaligned = dates_available.symmetric_difference(dates_expected)
-
-        self.assertEqual(len(dates_misaligned), 0)
-#pylint: enable=R0904
+    assert len(dates_misaligned) == 0
